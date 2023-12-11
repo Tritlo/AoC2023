bc19a70c3a91fc5830396ea5306ddcc44eaac8f1
Matthías Páll Gissurarson <mpg@mpg.is>
2023-12-11 19:14:18 +0000

    Day 10 part 2!

--- Day10/Day10.hs
+++ Day10/Day10.hs
@@ -25,11 +25,11 @@ data MapEntry = Ground
 
 parsePipe :: Char -> Pipe
 parsePipe '|' = [North, South]
-parsePipe '-' = [East, West]
 parsePipe 'L' = [North, East]
+parsePipe 'F' = [South, East]
+parsePipe '-' = [East, West]
 parsePipe 'J' = [North, West]
 parsePipe '7' = [South, West]
-parsePipe 'F' = [South, East]
 
 pipeType :: Pipe -> Char
 pipeType [North, South] = '|'
@@ -60,7 +60,7 @@ corresp South = North
 
 
 neighs :: (Int, Int) -> [(Int, Int)]
-neighs (x,y) = map (neigh (x,y)) [East, West, North, South]
+neighs (x,y) = map (neigh (x,y)) [West, South, East, North, South]
 
 --part1 :: [String] -> --(Map (Int,Int) MapEntry, (Int,Int))
 part1 strs = (1 + go coords Set.empty) `div` 2
@@ -68,7 +68,7 @@ part1 strs = (1 + go coords Set.empty) `div` 2
         ((coords,Start):[]) = Map.toList $ Map.filter (== Start) mp
         start_ns = neighs coords
         n_in_map c = case mp Map.!? c of
-                        Just Start -> mapMaybe f [North, South, East, West]
+                        Just Start -> mapMaybe f [West, South, East, North]
                         Just (Pipe xs) -> mapMaybe f xs
                         _ -> []
 
@@ -83,37 +83,117 @@ part1 strs = (1 + go coords Set.empty) `div` 2
           where ns = filter (not . flip Set.member seen) $ n_in_map c
 
 
-part2 strs = ( max_x, max_y, Set.size edges)
+part2 strs = case greenArea curve_1 of
+                ga | ga >= 0 -> ga - curveArea curve_1
+                ga -> abs ga - curveArea curve_2 -- we went the wrong way around.
   where mp = (Map.fromList . concat . zipWith (\i l -> map (\(j,e) -> ((i,j), e)) l ) [0..] . map (zip [0..] . map parseMap)) strs
         ((coords,Start):[]) = Map.toList $ Map.filter (== Start) mp
-        start_ns = neighs coords
+        pipe_types = Map.fromList $ map (\p -> (Set.fromList (parsePipe p), p)) "|LF-J7"
+        s_type = pipe_types Map.! (Set.fromList (map (fst .fst) $ n_in_map coords))
 
         n_in_map :: (Int,Int) -> [((Dir, [Dir]),(Int,Int))]
         n_in_map c = case mp Map.!? c of
-                        Just Start -> mapMaybe f [North, South, East, West]
+                        Just Start -> mapMaybe f [West, South, East, North]
                         Just (Pipe xs) -> mapMaybe f xs
                         _ -> []
-
            where f d = let nc = neigh c d
                        in case mp Map.!? nc of
                          Just (Pipe xs) | corresp d `elem` xs -> Just ((d,xs), nc)
-                         Just Start -> Just ((d,[]), nc)
+                         Just Start -> Just ((d,parsePipe s_type), nc)
                          _ -> Nothing
-        curve = go coords Set.empty
-        curve_set = Set.fromList $ map snd curve
+        [s_n_1,s_n_2] = n_in_map coords
+        -- We can go two ways around, clockwise and counter-clockwise.
+        -- Would be nice to be able to tell which is which, but it is easier
+        -- to just run it twice,
+        curve_1 = (s_n_1:go (snd s_n_1) (Set.singleton coords))
+        curve_2 = (s_n_2:go (snd s_n_2) (Set.singleton coords))
+
         go c !seen = case ns of
                       [] -> [head $ filter ((== coords) . snd) $ n_in_map c]
                       (n@(d,c'):_) -> n:go c' (Set.insert c seen)
-
           where ns = filter (not . flip Set.member seen . snd) $ n_in_map c
 
-        -- calculates the green area, but discretization is somehow wrong.
-        greenArea :: [((Dir,[Dir] ), (Int,Int))] -> Int
-        greenArea = greenArea' 0
-         where
-               greenArea' !a (c@((East,_), (y,_)):xs) = greenArea' (a+y) xs
-               greenArea' !a (c@((West,_), (y,_)):xs) = greenArea' (a-y) xs
-               greenArea' !a (c:xs) = greenArea' a xs
+        -- calculates the area within the curve as per Green's theorem.
+        -- '-' contributes two steps to the East or West, while 'F7LJ' only
+        -- contributes one. '|' contributes 0.
+        -- We calculate it in multiple of 4, imagining each point as the middle
+        -- of the square .|.
+        --               - -
+        --               .|.
+        -- We are going clockwise around the curve, as in the theorem.
+        -- If this returns a negative number, the curve was in the wrong
+        -- direction.
+        greenArea :: [((Dir,[Dir]), (Int,Int))] -> Int
+        greenArea = (`div` 4) . greenArea' 0
+         where greenArea' !a (c@((East,  p), (y,_)):xs) = greenArea' a' xs
+                  where a' = case pipeType p of
+                                '-' -> a + (4*y) -- xx
+                                                 -- ..
+                                'J' -> a + (2*y) -- x.
+                                                 -- ..
+                                '7' -> a + (2*y) -- xx
+                                                 -- .x
+               greenArea' !a (c@((West,  p), (y,_)):xs) = greenArea' a' xs
+                  where a' = case pipeType p of
+                                '-' -> a - (4*y) -- ..
+                                                 -- xx
+                                'F' -> a - (2*y) -- ..
+                                                 -- .x
+                                'L' -> a - (2*y) -- x.
+                                                 -- xx
+               greenArea' !a (c@((North, p), (y,_)):xs) = greenArea' a' xs
+                  where a' = case pipeType p of
+                                '|' -> a         -- .x
+                                                 -- .x
+                                'F' -> a + (2*y) -- xx
+                                                 -- x.
+                                '7' -> a - (2*y) -- ..
+                                                 -- x.
+               greenArea' !a (c@((South, p), (y,_)):xs) = greenArea' a' xs
+                  where a' = case pipeType p of
+                                '|' -> a         -- x.
+                                                 -- x.
+                                'L' -> a + (2*y) -- .x
+                                                 -- ..
+                                'J' -> a - (2*y) -- xx
+                                                 -- xx
+               greenArea' !a [] = a
+
+        -- Calculates the area that the curve itself occupies.
+        curveArea :: [((Dir,[Dir] ), (Int,Int))] -> Int
+        curveArea = (`div` 4) . greenArea' 0
+         where greenArea' !a (c@((East,  p), (y,_)):xs) = greenArea' a' xs
+                  where a' = case pipeType p of
+                                '-' -> a + 2 -- xx
+                                             -- ..
+                                'J' -> a + 1 -- x.
+                                             -- ..
+                                '7' -> a + 3 -- xx
+                                             -- .x
+               greenArea' !a (c@((West,  p), (y,_)):xs) = greenArea' a' xs
+                  where a' = case pipeType p of
+                                '-' -> a + 2 -- ..
+                                             -- xx
+                                'F' -> a + 1 -- ..
+                                             -- .x
+                                'L' -> a + 3 -- x.
+                                             -- xx
+               greenArea' !a (c@((North, p), (y,_)):xs) = greenArea' a' xs
+                  where a' = case pipeType p of
+                                '|' -> a + 2 -- .x
+                                             -- .x
+                                'F' -> a + 3 -- xx
+                                             -- x.
+                                '7' -> a + 1 -- ..
+                                             -- x.
+               greenArea' !a (c@((South, p), (y,_)):xs) = greenArea' a' xs
+                  where a' = case pipeType p of
+                                '|' -> a + 2 -- x.
+                                             -- x.
+                                'L' -> a + 1 -- .x
+                                             -- ..
+                                'J' -> a + 3 -- .x
+                                             -- xx
                greenArea' !a [] = a
 
 
@@ -121,18 +201,18 @@ part2 strs = ( max_x, max_y, Set.size edges)
 
 main :: IO ()
 main = do
-          readFile "example-1" >>= print . part1 . lines
-          putStrLn "---"
-          readFile "example-2" >>= print . part1 . lines
-          putStrLn "---"
-          readFile "example-3" >>= print . part1 . lines
+          -- readFile "example-1" >>= print . part1 . lines
+          -- readFile "example-2" >>= print . part1 . lines
+          -- readFile "example-3" >>= print . part1 . lines
           readFile "input" >>= print . part1 . lines
+          -- putStrLn "--"
         --   readFile "input" >>= print . part1 . lines
-          readFile "example-1" >>= mapM_ print . lines
-          readFile "example-1" >>= print .  part2 . lines
-          readFile "example-4" >>= mapM_ print . lines
-          readFile "example-4" >>= print .  part2 . lines
-          readFile "example-5" >>= mapM_ print . lines
-          readFile "example-5" >>= print .  part2 . lines
-          readFile "example-6" >>= mapM_ print . lines
-          readFile "example-6" >>= print .  part2 . lines
+          -- readFile "example-1" >>= mapM_ print . lines
+          -- readFile "example-1" >>= print .  part2 . lines
+          -- readFile "example-4" >>= mapM_ print . lines
+          -- readFile "example-4" >>= print .  part2 . lines
+          -- readFile "example-6" >>= mapM_ print . lines
+          -- readFile "example-6" >>= print .  part2 . lines
+          -- readFile "example-5" >>= mapM_ print . lines
+          -- readFile "example-5" >>= print .  part2 . lines
+          readFile "input" >>= print . part2 . lines
